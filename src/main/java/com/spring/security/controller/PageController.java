106b9978772ee053b4eccda789391fc396a96a15
kshitiz58 <kshitizbabik.37@gmail.com>
2024-05-16 20:41:16 +0545

    Spring Security 3.0 with Security Filtergit add .

---
+++ src/main/java/com/spring/security/controller/PageController.java
@@ -0,0 +1,21 @@
+package com.spring.security.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+	@GetMapping("/home")
+	public String getHomepage() {
+		return "home";
+	}
+	@GetMapping("/admin/home")
+	public String getAdminpage() {
+		return "admin";
+	}
+	@GetMapping("/user/home")
+	public String getUserpage() {
+		return "user";
+	}
+}
